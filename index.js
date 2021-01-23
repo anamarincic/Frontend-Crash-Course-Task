7c6c92fd3ee774bb0ebda17a771d8f342f4f3c5b
anig0ld <amarincic5@gmail.com>
2021-01-23 21:08:52 +0100

    final commit

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 document.querySelector("#basket").addEventListener("click", handle_click);
+document.querySelector("#btnDown").addEventListener("click", scroll_down);
+document.querySelector("#btnUp").addEventListener("click", scroll_up);
+
 
 function handle_click(){
+   var click= document.getElementById("list-items");
 
-     var click=document.getElementById("list-items");
-
-     if(click.style.display === "none") {
-            click.style.display = "block";
-     }else {
-            click.style.display = "none";
-        }
+   if(click.style.display === "none"){
+       click.style.display = "block";
+     }else{
+         click.style.display = "none";
+     }
 }
 
+function scroll_down(){
+    document.getElementById("products").scrollTop += 100;
+}
 
+function scroll_up(){
+    document.getElementById("products").scrollTop -= 100;
+}
